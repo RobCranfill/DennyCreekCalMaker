29bc2da262fb69f221f17ee960f31ec8fec729df
Rob Cranfill <robcranfill@robcranfill.net>
2021-03-11 11:22:29 -0800

    Works except for 1st entry!

--- dccc.py
+++ dccc.py
@@ -8,6 +8,63 @@ ALL_DATES_START_TIME = "T000001"
 MONTHS = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
 
 class Entry:
+    def parseDayAndMonth(self, in_str):
+        """
+        Input string is like "April 9 - 12" or "Aug 31 - Sept 2".
+
+        In which case return ["April", "09", "April", "12"] or ["Aug", "31", "Sept", "02"]
+        """
+
+        r0 = r1 = r2 = r3 = "??"
+
+        parts = in_str.split("-")
+
+        part0 = parts[0].strip(" ")
+        # print(f"part0 = '{part0}'")
+        month0 = part0.split(" ")[0]
+        sDay0  = part0.split(" ")[1]
+        if int(sDay0) < 10:
+            sDay0 = "0" + sDay0
+        try:
+            iMonth0 = MONTHS.index(month0) + 1
+            sMonth0 = str(iMonth0)
+            if iMonth0 < 10:
+                sMonth0 = "0" + sMonth0
+            # print(f"MONTH: {month0} = # {iMonth0}")
+            r0 = sMonth0
+            r1 = sDay0
+        except ValueError:
+            print(f"UNKNOWN MONTH '{month0}'")
+
+        part1 = parts[1].strip(" ")
+        # print(f"part1 = '{part1}'")
+
+        # month 2 may not exist
+        # parts1 can be like "13" or "Sept 02"
+        parts = part1.split(" ")
+        if len(parts) == 1:
+            sDay1  = part1.split(" ")[0]
+            month1 = month0
+        else:
+            sDay1  = part1.split(" ")[1]
+            month1 = parts[0]
+
+        if int(sDay1) < 10:
+            sDay1 = "0" + sDay1
+        try:
+            iMonth1 = MONTHS.index(month1) + 1
+            sMonth1 = str(iMonth1)
+            if iMonth1 < 10:
+                sMonth1 = "0" + sMonth1
+            # print(f"MONTH: {month1} = # {iMonth1}")
+            r2 = sMonth1
+            r3 = sDay1
+        except ValueError:
+            print(f"UNKNOWN MONTH '{month1}'")
+
+        return [r0, r1, r2, r3]
+
+
     def __init__(self, date_str, name):
         """
         Create an Entry object from the raw CVS strings.
@@ -28,30 +85,14 @@ class Entry:
                              HHMMSS
 
         """
-        self.date_start = "???"
-        self.date_end = "???"
         self.name = name
-
-        parts = date_str.split("-")
-        parts0 = parts[0].strip(" ")
-        print(f"parts0 = '{parts0}'")
-        month0 = parts0.split(" ")[0]
-        try:
-            iMonth0 = MONTHS.index(month0) + 1
-            sMonth0 = str(iMonth0)
-            if iMonth0 < 10:
-                sMonth0 = "0" + sMonth0
-            print(f"MONTH: {month0} = # {iMonth0}")
-            self.date_start = YEAR + sMonth0 + "dd" + ALL_DATES_START_TIME
-        except ValueError:
-            print(f"UNKNOWN MONTH '{month0}'")
-
-        # month 2 may not exist
-
+        r = self.parseDayAndMonth(date_str)
+        self.date_start = YEAR + r[1] + r[0] + ALL_DATES_START_TIME
+        self.date_end   = YEAR + r[3] + r[2] + ALL_DATES_START_TIME
 
 
 def parseInputFile(filepath):
-    print(f"reading {filepath}....")
+    # print(f"reading {filepath}....")
     result = []
     with open(filepath, newline='') as csvfile:
         # reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -63,7 +104,7 @@ def parseInputFile(filepath):
 
 def createVEventString(inputEntry):
     result = ""
-    # result = result + "BEGIN:VEVENT\n"
+    result = result + "BEGIN:VEVENT\n"
     # result = result + "UID:uid0101@tstdomain.com\n"
     # result = result + "DTSTAMP:19970714T170000Z\n"
     # result = result + "ORGANIZER;CN=Test User:MAILTO:test.user@tstdomain.com\n"
